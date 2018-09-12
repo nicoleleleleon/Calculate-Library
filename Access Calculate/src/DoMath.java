8241ca02d4180626062785c53a03d88bcd39a847
APCS2nd <APCS2nd@MHS-Dreyer-15>
2018-09-12 15:46:33 -0700

    Part 0 Version 12

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,12 @@
+//calls various methods in the calculate library
+//client code (code that uses other code)
+//@author Nicole Leon
+//@version 0, 9/12/18
+
+public class DoMath {
+	public static void main (String[] args) {
+		System.out.println(Calculate.square(-3)); //Gets info from Calculate class
+		
+	}
+}
+
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,11 @@
+// this class contains the methods to do various math related tasks
+//author: Nicole Leon
+//Version 0; 9/6/2018
+
+public class Calculate {
+//returns square of integer
+	public static int square(int number) { //int means return an integer
+	return number*number; //OR answer= number*number; return answer
+	
+	}
+}
