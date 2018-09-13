0373d1deb9aab5bf48a6228e0a5411d09615c8bb
APCS2nd <APCS2nd@MHS-Dreyer-15>
2018-09-13 09:50:29 -0700

    Part 2 Version 1

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -5,8 +5,21 @@
 
 public class DoMath {
 	public static void main (String[] args) {
-		System.out.println(Calculate.square(-3)); //Gets info from Calculate class
+		System.out.println("The square is " + Calculate.square(-3)); //Gets info from Calculate class
 		
+		System.out.println("The cube is " + Calculate.cube(-2));
+		
+		System.out.println("The averaege is " + Calculate.average(5,9));
+		
+		System.out.println("The degrees is " + Calculate.toDegrees(6));
+		
+		System.out.println("The Radians is " + Calculate.toRadians(180));
+		
+		System.out.println("The discriminant is " + Calculate.discriminant(1,5,6));
+		
+		System.out.println("The improper fraction is " + Calculate.toImproperFrac(6,6,6));
+	
+	
 	}
 }
 
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -8,4 +8,32 @@ public class Calculate {
 	return number*number; //OR answer= number*number; return answer
 	
 	}
+	
+	public static int cube(int number) { //cube returns cube of value passed
+		return number*number*number;
+	}
+	
+	
+	public static double average(int num1,int num2) {
+		return (num1 + num2)/2.0;
+		
+	}
+	
+	public static double toDegrees(int number) {
+		return number*180/3.14159;
+		
+	}
+	
+	public static double toRadians(int number) {
+		return number*3.14159/180; 
+		
+	}
+	
+	public static double discriminant(int numA,int numB,int numC) {
+		return (numB * numB) - 4*(numA * numC);
+	}
+	
+	public static int discriminant(int numWhole,int numNum,int numDen) {
+		
+	}
 }
